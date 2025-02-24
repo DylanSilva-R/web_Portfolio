b36969f5bfaab8ad0405b31e6b1ca1dfa9511c65
DylanSilva-R <dylansilvarw@gmail.com>
2025-02-24 00:03:48 -0500

    Major fixes

--- front_End/js/script.js
+++ front_End/js/script.js
@@ -1,18 +1,12 @@
 
-const scenes = document.querySelectorAll('.scene');
 
-function screenTransition(index)
+
+function toGithubProjectsPage()
 {
-    // Applying 
-    const sceneSize = scenes.length;
+    location.href = "githubExperience.html";
+}
 
-    for(let i = 0; i < sceneSize; i++)
-    {
-
-        if(scenes[i].classList.contains('active'))
-        {
-            scenes[i].classList.remove('active');
-            scenes[index].classList.add('active');
-        }
-    }
+function toHomePage()
+{
+    location.href = "index.html";
 }
